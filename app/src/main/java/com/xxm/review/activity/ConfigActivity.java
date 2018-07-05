8abc6935272ee9dc33b8439e4e4891b74b7495e9
xuxiaoming <1229717912@qq.com>
2018-07-05 16:24:56 +0800

    注解测试

--- app/src/main/java/com/xxm/review/activity/ConfigActivity.java
+++ app/src/main/java/com/xxm/review/activity/ConfigActivity.java
@@ -1,12 +1,16 @@
 package com.xxm.review.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.xxm.review.R;
 import com.xxm.review.domain.InfoConfig;
 
 public class ConfigActivity extends AppCompatActivity {
+
+    private static final String TGA = ConfigActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,14 @@ public class ConfigActivity extends AppCompatActivity {
                 .setAge(10)
                 .setAddress("address");
 
+
+        test("1234456789");
+
+    }
+
+
+    private void test(@NonNull String name) {
+        Log.d(TGA, name);
     }
 
 }
