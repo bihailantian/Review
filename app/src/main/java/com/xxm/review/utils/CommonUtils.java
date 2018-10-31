06282862e1795d15a0bb9704c882d2a5c9f7f6fd
xuxiaoming <1229717912@qq.com>
2018-10-31 16:55:25 +0800

    添加获取正在运行桌面包名方法

--- app/src/main/java/com/xxm/review/utils/CommonUtils.java
+++ app/src/main/java/com/xxm/review/utils/CommonUtils.java
@@ -2,11 +2,33 @@ package com.xxm.review.utils;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.graphics.PixelFormat;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 public class CommonUtils {
+
+
+    /**
+     * 获取正在运行桌面包名（注：存在多个桌面时且未指定默认桌面时，该方法返回Null,使用时需处理这个情况）
+     */
+    public String getLauncherPackageName(Context context) {
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        final ResolveInfo res = context.getPackageManager().resolveActivity(intent, 0);
+        if (res.activityInfo == null) {
+            // should not happen. A home is always installed, isn't it?
+            return null;
+        }
+        if (res.activityInfo.packageName.equals("android")) {
+            // 有多个桌面程序存在，且未指定默认项时；
+            return null;
+        } else {
+            return res.activityInfo.packageName;
+        }
+    }
 
 
     /**
