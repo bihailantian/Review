a3747ac0861a8df961afb1a2504da7344b5a19f2
xuxiaoming <1229717912@qq.com>
2018-12-24 21:56:16 +0800

    JobService的初步了解

--- app/src/main/java/com/xxm/review/activity/JobServiceActivity.java
+++ app/src/main/java/com/xxm/review/activity/JobServiceActivity.java
@@ -1,15 +1,24 @@
 package com.xxm.review.activity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.xxm.review.R;
 import com.xxm.review.base.BaseActivity;
+import com.xxm.review.service.JobSchedulerService;
 
 /**
  * JobService和JobScheduler测试
  */
 public class JobServiceActivity extends BaseActivity {
+
+    private static final String TAG = JobServiceActivity.class.getSimpleName();
+    private int jobId = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,27 @@ public class JobServiceActivity extends BaseActivity {
     }
 
     public void startJobSchedulerService(View view) {
+        Log.d(TAG, "startJobSchedulerService");
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        ComponentName componentName = new ComponentName(this, JobSchedulerService.class);
+        JobInfo.Builder builder = new JobInfo.Builder(jobId++, componentName);
+        //设置JobService执行的最小延时时间
+        builder.setMinimumLatency(1000 * 3);
+        //设置JobService执行的最晚时间
+        builder.setOverrideDeadline(1000 * 15);
+        /*
+         * 联网状态（
+         * NETWORK_TYPE_NONE--不需要网络，
+         * NETWORK_TYPE_ANY--任何可用网络，
+         * NETWORK_TYPE_UNMETERED--不按用量计费的网络）
+         */
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NONE);
 
+        builder.setRequiresDeviceIdle(false);//是否要求设备为idle状态
+        builder.setRequiresCharging(false);//是否要设备为充电状态
+
+        if (jobScheduler != null) {
+            jobScheduler.schedule(builder.build()); //至少要有一个约束，否则抛出异常
+        }
     }
 }
--- app/src/main/java/com/xxm/review/service/JobSchedulerService.java
+++ app/src/main/java/com/xxm/review/service/JobSchedulerService.java
@@ -3,16 +3,39 @@ package com.xxm.review.service;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.util.Log;
+import android.widget.Toast;
 
+/**
+ * JobService 需要添加权限android.permission.BIND_JOB_SERVICE（在
+ *
+ * <pre class="prettyprint">
+ *     &#60;service android:name="MyJobService"
+ *              android:permission="android.permission.BIND_JOB_SERVICE" &#62;
+ *         ...
+ *     &#60;/service&#62;
+ * </pre>
+ *
+ *
+ * 节点添加）
+ */
 public class JobSchedulerService extends JobService {
+
+    private static final String TAG = JobSchedulerService.class.getSimpleName();
 
     @Override
     public boolean onStartJob(JobParameters params) {
+        Log.d(TAG,"onStartJob");
+        Toast.makeText(this, "start job:" + params.getJobId(), Toast.LENGTH_SHORT).show();
+        jobFinished(params, false);//任务执行完后调用jobFinsih通知系统释放相关资源
         return false;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
+        Log.d(TAG,"onStopJob");
         return false;
     }
+
+
 }
