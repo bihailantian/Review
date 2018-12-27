acc98b2eb2da773b29c23c4e10158ed538356d0c
xuxiaoming <1229717912@qq.com>
2018-12-27 17:15:44 +0800

    修复空指针的bug

--- app/src/main/java/com/xxm/review/utils/ServerUtils.java
+++ app/src/main/java/com/xxm/review/utils/ServerUtils.java
@@ -8,17 +8,26 @@ import java.util.List;
 
 public class ServerUtils {
 
+
+    /**
+     * 判断Service是否正在运行
+     *
+     * @param context     上下文
+     * @param serviceName Service 类全名
+     * @return true 表示正在运行，false 表示没有运行
+     */
     public static boolean isServiceRunning(Context context, String serviceName) {
         // 校验服务是否还活着
         ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<RunningServiceInfo> infos = am.getRunningServices(100);
-        for (RunningServiceInfo info : infos) {
-            String name = info.service.getClassName();
-            if (name.equals(serviceName)) {
-                return true;
+        if (am != null) {
+            List<RunningServiceInfo> serviceInfoList = am.getRunningServices(100);
+            for (RunningServiceInfo info : serviceInfoList) {
+                String name = info.service.getClassName();
+                if (name.equals(serviceName)) {
+                    return true;
+                }
             }
         }
-
         return false;
 
     }
