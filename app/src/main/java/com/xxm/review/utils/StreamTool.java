b1e8ffde0611d3919714c6684633c26c9015140b
xuxiaoming <1229717912@qq.com>
2019-05-13 15:22:39 +0800

    String写入文件工具方法

--- app/src/main/java/com/xxm/review/utils/StreamTool.java
+++ app/src/main/java/com/xxm/review/utils/StreamTool.java
@@ -1,6 +1,10 @@
 package com.xxm.review.utils;
 
+import android.os.Environment;
+
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 
 /**
@@ -22,6 +26,51 @@ public class StreamTool {
         }
         inStream.close();
         return outStream.toByteArray();
+    }
+
+
+    public static void getString(String str) {
+
+        String filePath = null;
+
+        boolean hasSDCard = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+
+        if (hasSDCard) {
+
+            filePath =Environment.getExternalStorageDirectory().toString() + File.separator +"hello.txt";
+
+        } else {
+
+            filePath = Environment.getDownloadCacheDirectory().toString() + File.separator + "hello.txt";
+        }
+
+
+        try {
+
+            File file = new File(filePath);
+
+            if (!file.exists()) {
+
+                File dir = new File(file.getParent());
+
+                dir.mkdirs();
+
+                file.createNewFile();
+
+            }
+
+            FileOutputStream outStream = new FileOutputStream(file);
+
+            outStream.write(str.getBytes());
+
+            outStream.close();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
     }
 
 }
