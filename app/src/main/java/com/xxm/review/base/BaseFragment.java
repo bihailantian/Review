939be83edea49c2c00f5100f839a40f99aebfd8c
xuxiaoming <1229717912@qq.com>
2019-07-02 14:26:00 +0800

    添加ViewTooltip工具类

--- app/src/main/java/com/xxm/review/base/BaseFragment.java
+++ app/src/main/java/com/xxm/review/base/BaseFragment.java
@@ -1,7 +1,73 @@
 package com.xxm.review.base;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Fragment的基类
+ */
+public abstract class BaseFragment extends Fragment {
+
+    protected static final String TAG =  BaseFragment.class.getSimpleName();
 
 
-public class BaseFragment extends Fragment {
+    protected interface OnUIPreListener {
+        public void onUIPre(BaseFragment fragment);
+    }
+
+    public String tag;
+    /** fragment content view cache */
+    protected View mContainer;
+    private int mId;
+
+    /**
+     * 
+     * Called to have the fragment instantiate its user interface view
+     * 
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return Return the View for the fragment's UI
+     */
+    protected abstract View onInflaterContent(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState);
+
+    /**
+     * Call before switching animation
+     * 
+     * @param gainSelect
+     */
+    protected abstract void onFragmentSeletedPre(boolean gainSelect);
+
+    /**
+     * Completely visible or invisible.
+     * 
+     * @param gainShow
+     */
+    public abstract void onFragmentShowChanged(boolean gainShow);
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (mContainer == null) {
+            mContainer = onInflaterContent(inflater, container, savedInstanceState);
+        }
+        return mContainer;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mContainer != null) {
+            ViewGroup parent = (ViewGroup) mContainer.getParent();
+            if (parent != null) {
+                parent.removeView(mContainer);
+            }
+        }
+    }
+
+    public View getContainer() {
+        return mContainer;
+    }
 }
