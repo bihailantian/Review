1c3362bce4cbcc9aaee0935fd2c7c0a30f67c179
xuxiaoming <1229717912@qq.com>
2020-09-27 11:53:18 +0800

    getGitRevision更新

---
+++ app/src/main/java/com/xxm/review/PathPrint.java
@@ -0,0 +1,43 @@
+package com.xxm.review;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+
+public class PathPrint {
+
+
+    private static final String TAG = "PathPrint";
+
+    public static void printPath(Activity activity){
+        Log.d(TAG, "getPath()= " +  Environment.getExternalStorageDirectory().getPath() ); //<p>  /storage/emulated/0
+        Log.d(TAG, "getPackageName()= " + activity.getPackageName());
+        Log.d(TAG, "getApplicationInfo().packageName= " + activity.getApplicationInfo().packageName);
+        Log.d(TAG, "getApplicationInfo().processName= " + activity.getApplicationInfo().processName);
+        Log.d(TAG, "getApplication().getPackageName()= " + activity.getApplication().getPackageName());
+        Log.d(TAG, "目录= " + Environment.getExternalStorageDirectory().getAbsolutePath()); //<p>  /storage/emulated/0
+        Log.d(TAG, "sd卡: " + Environment.getExternalStorageState());
+        Log.d(TAG, "sd卡: " + Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)); //判断sd卡是否可读
+        Log.d(TAG, "文件路径: " + Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Review"); //<p>   /storage/emulated/0/Review
+        Log.d(TAG, "文件路径: " + Environment.getExternalStorageDirectory()); //<p>  /storage/emulated/0
+        Log.d(TAG, "Build.VERSION.SDK_INT= " + Build.VERSION.SDK_INT);
+        try {
+            Log.d(TAG, "getApplicationInfo().packageName= " + activity.getPackageManager().getPackageInfo(activity.getPackageName(), 0).versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Log.d(TAG,"Environment.DIRECTORY_DOWNLOADS："+activity.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS));  //<p> /storage/emulated/0/Android/data/com.xxm.review/files/Download
+            Log.d(TAG,"getDataDirectory()："+Environment.getDataDirectory());  //<p> /data
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
