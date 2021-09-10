1283b3269e8078f256dc64b39a40f19a5b3331bf
xuxiaoming <1229717912@qq.com>
2021-09-10 15:55:57 +0800

    Notification状态栏通知

---
+++ app/src/main/java/com/xxm/review/activity/NotificationActivity.java
@@ -0,0 +1,24 @@
+package com.xxm.review.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.xxm.review.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+
+    public void showProgressNotification(View view) {
+
+
+
+    }
+
+
+}
