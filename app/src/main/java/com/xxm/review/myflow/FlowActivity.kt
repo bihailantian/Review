2fcdc54c0f73f9b8cdfb856f9b4130d6a9f563ba
xuxiaoming <1229717912@qq.com>
2024-07-10 10:37:37 +0800

    连续依次弹窗

--- app/src/main/java/com/xxm/review/myflow/FlowActivity.kt
+++ app/src/main/java/com/xxm/review/myflow/FlowActivity.kt
@@ -2,8 +2,11 @@ package com.xxm.review.myflow
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.xxm.review.R
 import com.xxm.review.databinding.ActivityFlowBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,6 +18,8 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 class FlowActivity : AppCompatActivity() {
     private val TAG = "FlowActivity-"
@@ -28,6 +33,7 @@ class FlowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityFlowBinding.inflate(layoutInflater)
+        setTheme(R.style.FlowActivityTheme)
         setContentView(mBinding.root)
         Log.d(TAG, " main threadId=${Thread.currentThread().id}")
         setupFlow()
@@ -70,11 +76,34 @@ class FlowActivity : AppCompatActivity() {
                 flow.collect(object : FlowCollector<Int> {
                     override suspend fun emit(value: Int) {
                         Log.d(TAG, "setupClicks: $value, threadId=${Thread.currentThread().id}")
+                        Toast.makeText(this@FlowActivity, "setupClicks: $value, threadId=${Thread.currentThread().id}", Toast.LENGTH_SHORT).show()
                     }
 
                 })
             }
         }
+
+        mBinding.buttonDialog.setOnClickListener {
+            lifecycleScope.launch {
+                showDialog("签到活动", "签到领10000币") // 直到dialog被关闭, 才会继续运行下一行
+                showDialog("新手任务", "做任务领20000币") // 直到dialog被关闭, 才会继续运行下一行
+                showDialog("首充奖励", "首充6元送神装")
+            }
+
+        }
+    }
+
+    suspend fun showDialog(title: String, content: String) = suspendCancellableCoroutine<Unit> { continuation ->
+        MaterialAlertDialogBuilder(this)
+                .setTitle(title)
+                .setMessage(content)
+                .setPositiveButton("我知道了") { dialog, which ->
+                    dialog.dismiss()
+                }
+                .setOnDismissListener {
+                    continuation.resume(Unit)
+                }
+                .show()
     }
 
 
